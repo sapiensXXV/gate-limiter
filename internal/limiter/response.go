0f35073c562db894bff7995301456c8b251a152c
Jaehoon-So <jhspacelover@naver.com>
2025-07-22 05:05:57 +0900

    fix: redis client interface refactoring

--- internal/limiter/response.go
+++ internal/limiter/response.go
@@ -1,23 +1,59 @@
 package limiter
 
 import (
+	"gate-limiter/pkg/redisclient"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
+)
+
+const (
+	XRateLimitRemaining  = "X-RateLimit-Remaining"
+	XRateLimitReset      = "X-RateLimit-Reset"
+	XRateLimitRetryAfter = "X-RateLimit-Retry-After"
 )
 
 type LimitResponder interface {
 	RespondRateLimitExceeded(w http.ResponseWriter, r *http.Request, remaining int)
 }
 
-type HttpLimitResponder struct{}
+type HttpLimitResponder struct {
+	CalcRetryAfter func(key string) int
+	RedisClient    redisclient.RedisClient
+}
+
+func NewHttpLimitResponder(redisClient redisclient.RedisClient) *HttpLimitResponder {
+	h := &HttpLimitResponder{}
+	h.CalcRetryAfter = h.defaultCalculateRetryAfter
+	h.RedisClient = redisClient
+	return h
+}
 
 func (h *HttpLimitResponder) RespondRateLimitExceeded(w http.ResponseWriter, r *http.Request, remaining int) {
 	ipAddress := r.Header.Get(XForwardedFor)
 	key := MakeRateLimitKey(ipAddress, "comment") // 이부분도 언젠가는 yml로 받아서 처리하길
-	retryAfter := CalculateRetryAfter(key)
+	retryAfter := h.CalcRetryAfter(key)
 
-	w.Header().Set("X-Ratelimit-Remaining", strconv.Itoa(remaining))
-	w.Header().Set("X-Ratelimit-Limit", strconv.Itoa(AllowedCount))
-	w.Header().Set("X-Ratelimit-Retry-After", strconv.Itoa(retryAfter))
+	w.Header().Set(XRateLimitRemaining, strconv.Itoa(remaining))
+	w.Header().Set(XRateLimitReset, strconv.Itoa(AllowedCount))
+	w.Header().Set(XRateLimitRetryAfter, strconv.Itoa(retryAfter))
 	w.WriteHeader(http.StatusTooManyRequests) // HTTP 429 (too many requests)
+}
+
+func (h *HttpLimitResponder) defaultCalculateRetryAfter(key string) int {
+	oldest, err := h.RedisClient.GetOldestEntry(key)
+	if err != nil {
+		log.Printf("fail to get oldest entry on key=[%s]\n", key)
+	}
+
+	oldestTime := time.Unix(int64(oldest.Score), 0)
+	retryAt := oldestTime.Add(time.Minute * 1)
+	now := time.Now()
+
+	wait := retryAt.Sub(now).Seconds()
+	if wait < 0 {
+		return 0
+	}
+	return int(wait)
 }
--- pkg/redisclient/client.go
+++ pkg/redisclient/client.go
@@ -1,13 +1,75 @@
 package redisclient
 
 import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
-func Set(key string, value interface{}, expiration time.Duration) error {
-	return Rdb.Set(ctx, key, value, expiration).Err()
+type RedisClient interface {
+	RemoveOldEntries(key string, cutoff time.Time) error
+	AddToSortedSet(key, member string, score time.Time) error
+	GetZSetSize(key string) int
+	GetOldestEntry(key string) (redis.Z, error)
 }
 
-func Get(key string) (interface{}, error) {
-	return Rdb.Get(ctx, key).Result()
+type DefaultRedisClient struct {
+	ctx context.Context
+	rdb *redis.Client
+}
+
+var _ RedisClient = (*DefaultRedisClient)(nil)
+
+func NewDefaultRedisClient() *DefaultRedisClient {
+	dbValue, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		log.Fatal("redis initialization fail")
+	}
+	rc := &DefaultRedisClient{}
+	rc.ctx = context.Background()
+	rc.rdb = redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+		Password: os.Getenv("REDIS_PASSWORD"), // no password
+		DB:       dbValue,
+	})
+
+	if err := rc.rdb.Ping(rc.ctx).Err(); err != nil {
+		log.Fatal("redis connection fail")
+	}
+	log.Println("redis connection success")
+
+	return rc
+}
+
+func (d *DefaultRedisClient) RemoveOldEntries(key string, cutoff time.Time) error {
+	score := float64(cutoff.Unix())
+	return d.rdb.ZRemRangeByScore(ctx, key, "0", strconv.FormatFloat(score, 'f', -1, 64)).Err()
+}
+
+func (d *DefaultRedisClient) AddToSortedSet(key, member string, t time.Time) error {
+	score := float64(t.Unix())
+	z := &redis.Z{
+		Score:  score,
+		Member: member,
+	}
+	return d.rdb.ZAdd(ctx, key, *z).Err()
+}
+
+func (d *DefaultRedisClient) GetZSetSize(key string) int {
+	size, err := d.rdb.ZCard(ctx, key).Result()
+	if err != nil {
+		log.Println("redis: get zset size fail")
+	}
+	return int(size)
+}
+
+func (d *DefaultRedisClient) GetOldestEntry(key string) (redis.Z, error) {
+	vals, err := d.rdb.ZRangeWithScores(ctx, key, 0, 0).Result()
+	if err != nil {
+		log.Println("redis: get oldest entry fail")
+	}
+	return vals[0], err
 }
