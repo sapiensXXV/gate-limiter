347c1d853f82d4440afc2df1957527b5196b0b84
Jaehoon-So <jhspacelover@naver.com>
2025-08-13 02:19:12 +0900

    test: 변경된 키 생성기 테스트

--- internal/limiter/util/keygen_test.go
+++ internal/limiter/util/keygen_test.go
@@ -1,12 +1,15 @@
 package util
 
 import (
+	"gate-limiter/config/settings"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIpKeyGenerator_Make(t *testing.T) {
-	generator := IpKeyGenerator{}
+	//generator := IpKeyGenerator{}
+	config := &settings.RateLimiterConfig{Strategy: "token_bucket"}
+	generator := NewIpKeyGenerator(*config)
 	result := generator.Make("11.11.11.11", "comment")
-	assert.Equal(t, result, "11.11.11.11:comment")
+	assert.Equal(t, result, "token_bucket:11.11.11.11:comment")
 }
