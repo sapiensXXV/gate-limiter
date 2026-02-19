51cb3e4508c133e950343d739a3ccf1c50845ce3
sapiensnoah <noah.so@kakaocorp.com>
2026-02-19 10:56:07 +0900

    fix: LeakyBucket 큐 구조 변경

--- internal/limiter/types/bucket.go
+++ internal/limiter/types/bucket.go
@@ -12,9 +12,16 @@ func NewTokenBucket(token int) *TokenBucket {
 	return &TokenBucket{Token: token, LastRefillTime: time.Now()}
 }
 
+// LeakyQueueItem leaky-bucket 큐에 들어가는 항목
+// 응답을 비동기로 쓰지 않기 위해서 Writer는 저장하지 않는다
+type LeakyQueueItem struct {
+	Done       chan struct{}
+	EnqueuedAt time.Time
+}
+
 // LeakyBucket 누출버킷을 표현하는 구조체
 type LeakyBucket struct {
-	Queue           chan QueuedRequest
+	Queue           chan *LeakyQueueItem
 	BucketSize      int
 	LastProcessTime time.Time
 }
